ca4aaeb8d80f05f5430d4310ce48c3fa3fe2d603
jpgambrell <john.gambrell@gmail.com>
2017-12-18 13:04:17 -0600

    updated for Swift 4

--- Package.swift
+++ Package.swift
@@ -1,21 +1,26 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
-    name: "BeeHiveServer",
-    targets: [
-        Target(name: "App"),
-        Target(name: "Run", dependencies: ["App"]),
+    name: "BeehiveServer",
+    products: [
+        .library(name: "App", targets: ["App"]),
+        .executable(name: "Run", targets: ["Run"])
     ],
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
-        .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
-        .Package(url: "https://github.com/vapor/postgresql.git", majorVersion: 2)
+        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
+        .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
+        .package(url: "https://github.com/vapor/postgresql.git", .upToNextMajor(from: "2.0.0"))
     ],
-    exclude: [
-        "Config",
-        "Database",
-        "Localization",
-        "Public",
-        "Resources",
+    targets: [
+        .target(name: "App", dependencies: ["Vapor", "FluentProvider", "PostgreSQL"],
+                exclude: [
+                    "Config",
+                    "Public",
+                    "Resources",
+                ]),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App", "Testing"])
     ]
 )
